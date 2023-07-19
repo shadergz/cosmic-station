d3b464656398d72732d7f9ed0b9e8ce9c46b7f4c
Gabriel Correia <gabrielcorreia01012020@hotmail.com>
2023-07-19 15:52:00 -0300

    Applies `MaterialYou` and adds a simple `topbar`

--- app/src/main/java/emu/zenith/MainActivity.kt
+++ app/src/main/java/emu/zenith/MainActivity.kt
@@ -1,7 +1,10 @@
 package emu.zenith
 
+import android.content.pm.PackageManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.view.WindowCompat
 import emu.zenith.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -13,7 +16,22 @@ class MainActivity : AppCompatActivity() {
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        WindowCompat.setDecorFitsSystemWindows(window, true)
+        binding.homeToolbar.apply {
+            title = resources.getString(R.string.app_name)
+            subtitle = getAppVersion()
+        }
     }
 
-
+}
+fun MainActivity.getAppVersion(): String {
+    applicationContext.packageManager.let {
+        val packageInfo = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            packageManager.getPackageInfo(packageName, PackageManager.PackageInfoFlags.of(0))
+        } else {
+            @Suppress("deprecation") packageManager.getPackageInfo(packageName, 0)
+        }
+        return packageInfo.versionName
+    }
 }
--- app/src/main/java/emu/zenith/ZenithApplication.kt
+++ app/src/main/java/emu/zenith/ZenithApplication.kt
@@ -6,6 +6,7 @@ import com.google.android.material.color.DynamicColors
 class ZenithApplication : Application() {
     override fun onCreate() {
         super.onCreate()
+        // Applies dynamic colors to your application
         DynamicColors.applyToActivitiesIfAvailable(this)
 
         System.loadLibrary("zenith")
