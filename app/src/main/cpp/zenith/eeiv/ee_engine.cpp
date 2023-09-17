8dec7a823184eb1764e97246266f3deb51ff38a3
Gabriel Correia <gabrielcorreia01012020@hotmail.com>
2023-09-16 18:34:05 -0700

    EE: Clearing all GPRs of the EE upon reset, cache in progress

--- app/src/main/cpp/zenith/eeiv/ee_engine.cpp
+++ app/src/main/cpp/zenith/eeiv/ee_engine.cpp
@@ -20,6 +20,19 @@ namespace zenith::eeiv {
         // The BIOS should be around here somewhere
         m_eePC = 0xbfc00000;
 
+        for (u8 regRange{}; regRange != countOfGPRs; regRange += 8) {
+            // Writing 256 bits (32 bytes) per write call
+            static auto zeGPRx2ByLane = [](eeRegister* regIndex, const u8 posIn) {
+                static const uint64x1x4_t zero{};
+                vst1_u64_x4(reinterpret_cast<u64*>(regIndex) + posIn, zero);
+            };
+
+            zeGPRx2ByLane(m_GPRs, regRange + 0);
+            zeGPRx2ByLane(m_GPRs, regRange + 2);
+            zeGPRx2ByLane(m_GPRs, regRange + 4);
+            zeGPRx2ByLane(m_GPRs, regRange + 6);
+        }
+
         // The first register of the EE is register zero, and its value is always zero. Any attempt
         // to write to it is discarded by default
         m_GPRs[0].qw = 0;
