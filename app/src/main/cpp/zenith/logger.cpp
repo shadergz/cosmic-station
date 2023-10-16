55293447154b16a77594e9b75b0d597f7bbfb44b
Gabriel Correia <gabrielcorreia01012020@hotmail.com>
2023-10-16 16:23:56 -0700

    Isolating `Perfetto` within our addon library (vendor)

--- app/src/main/cpp/zenith/logger.cpp
+++ app/src/main/cpp/zenith/logger.cpp
@@ -1,13 +1,9 @@
-#include <perfetto.h>
 #include <logger.h>
+#include <perfetto.h>
 
-PERFETTO_DEFINE_CATEGORIES(
-    perfetto::Category("launch")
-        .SetDescription("Metrics obtained through the launch of the application"));
-
-// It will expand into a structure containing all possible events,
-// which need to be static in the binary sections
-PERFETTO_TRACK_EVENT_STATIC_STORAGE();
+namespace addons {
+    extern void enbPerfettoProfiler(perfetto::TracingInitArgs& appArgs);
+}
 
 namespace zenith {
     void verifyRtCheck(bool condition, std::function<void()> func) {
@@ -17,10 +13,9 @@ namespace zenith {
 
     GlobalLogger::GlobalLogger() {
         perfetto::TracingInitArgs args;
-
         // Write all records to the `traced` daemon, to improve speed and accuracy during tracing
         args.backends |= perfetto::kSystemBackend;
-        perfetto::Tracing::Initialize(args);
-        perfetto::TrackEvent::Register();
+
+        addons::enbPerfettoProfiler(args);
     }
 }
