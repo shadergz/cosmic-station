6c69146d26ab0710e8183ac3601151ee343c84d0
Gabriel Correia <gabrielcorreia01012020@hotmail.com>
2023-10-18 15:32:09 -0300

    Just little fixes that I forgot to add in the last commit

--- app/src/main/cpp/zenith/eeiv/cop_cache.cpp
+++ app/src/main/cpp/zenith/eeiv/cop_cache.cpp
@@ -35,7 +35,7 @@ namespace zenith::eeiv {
 
         [[unlikely]] if (line->tags[0] != logical && line->tags[1] != logical) {
             throw fatalError("(Cop0): No portion of the cache line {} was properly selected! "
-                "tags[0] = {}, tags[1] = {} ", logical, line->tags[0], line->tags[1]);
+                "tags[0] = {}, tags[1] = {}", logical, line->tags[0], line->tags[1]);
         }
 
         auto cacheData{eeCore.tableRead<os::machVec128>(address)};
