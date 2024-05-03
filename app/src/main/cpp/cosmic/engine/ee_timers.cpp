a4384cac08abeccd985dbfc2e4c841bf6c5edaa8
Correia <gabrielcorreia01012020@hotmail.com>
2024-05-02 21:04:55 -0300

    `EE`: Fixes the event handler of EE timers

--- app/src/main/cpp/cosmic/engine/ee_timers.cpp
+++ app/src/main/cpp/cosmic/engine/ee_timers.cpp
@@ -1,47 +1,78 @@
 #include <common/except.h>
 #include <engine/ee_timers.h>
 #include <engine/ee_intc.h>
+#include <console/intc.h>
 
 namespace cosmic::engine {
     EeTimers::EeTimers(std::shared_ptr<vm::Scheduler>& solver,
         std::shared_ptr<console::IntCInfra>& inte) :
             scheduler(solver), intc(inte) {
+        std::memset(&timers, 0, sizeof(timers));
+
+    }
+    // PAL:  312 scanlines per frame (VBOFF: 286 | VBON: 26)
+    // NTSC: 262 scanlines per frame (VBOFF: 240 | VBON: 22)
+
+    // PAL:  9436 BUSCLK cycles per scanline
+    // NTSC: 9370 BUSCLK cycles per scanline
+    void EeTimers::sysCtrlGate(bool vb, bool high) {
+        for (u8 ti{}; ti < timers.size(); ti++) {
+            Ref<HwTimer> clocked{timers[ti]};
+            if (!(clocked->gate & 0xf0) && clocked->withVbSync != vb)
+                continue;
+
+            switch (clocked->gateMode) {
+            case ActivateGate:
+                clocked->gate |= high ? 0xf : 0; break;
+            case ResetGateWhenHigh:
+                if (high) clocked->count = 0; break;
+            case ResetGateWhenLow:
+                if (!high) clocked->count = 0;
+                clocked->gate |= 0xf;
+                break;
+            case ResetGateWithDiffer:
+                // 3=Reset counter for high<->low gate transitions
+                if (((clocked->gate & 0xf) == 0xf) == high)
+                    continue;
+                clocked->count = 0;
+                clocked->gate &= ~0xf;
+            }
+        }
     }
     void EeTimers::resetTimers() {
         for (u8 chronos = {}; chronos != timers.size(); chronos++) {
-            timers.at(chronos) = {};
             // Not necessary perhaps, it will depend on the implementation
+            timers.at(chronos) = {};
             timers[chronos].count = {};
         }
-        raiseEvent = scheduler->createSchedTick(false,[this](u8 position, bool ov) {
-            timerReached(position, ov);
+        raiseEvent = scheduler->createSchedTick(false,
+            [this](u8 rice, bool over) {
+                raiseClkTrigger(rice, over);
         });
-        for (u8 idx = {}; idx != timers.size(); idx++) {
-            timers[idx].callId = scheduler->addTimer(raiseEvent, 0xffff,
-                std::make_tuple(idx, false));
-        }
+        for (u8 idx = {}; idx != timers.size(); idx++)
+            timers[idx].callId = scheduler->addTimer(
+                raiseEvent, 0xffff,std::make_tuple(idx, false));
     }
-    void EeTimers::timerReached(u8 raised, bool overflow) {
+    void EeTimers::raiseClkTrigger(u8 raised, bool overflow) {
         // This function is responsible for enabling the clock timer exception;
         // it should ensure that the 'overflow' flag is enabled
         static u8 base{};
 
         base = engine::T0 + raised;
-        const auto timer{std::addressof(timers.at(raised))};
+        const auto ticked{std::addressof(timers.at(raised))};
         if (!overflow) {
-            const bool compare{timer->ctrl.trap.compare};
-            if (compare) {
-                timer->ctrl.trap.compare = true;
-                intc->trapIrq(console::EeInt, base);
+            if (ticked->clearCountWithDiff) {
+                ticked->count = {};
             }
-        }
-        const bool woutOverflow{!timer->ctrl.trap.overflow};
-        const bool shouldTrap{woutOverflow && overflow};
-        if (shouldTrap) {
-            timer->ctrl.trap.overflow = true;
+            if ((ticked->trap.compareMask & 0xf) != 0xf) {
+                ticked->trap.compareMask |= 0xf;
+
+            }
+        } else if ((ticked->trap.overflowMask & 0xf) != 0xf) {
+            ticked->trap.overflowMask |= 0xf;
+            intc->trapIrq(console::EeInt, base);
             intc->trapIrq(console::EeInt, base);
         }
     }
 }
-
 
--- app/src/main/cpp/cosmic/engine/ee_timers.h
+++ app/src/main/cpp/cosmic/engine/ee_timers.h
@@ -7,37 +7,57 @@ namespace cosmic::vm {
     class Scheduler;
 }
 namespace cosmic::engine {
+    struct TimerInt {
+        // Causes interruptions in the CPU when enabled
+        // TN_COMP
+        u16 tnComp;
+        // Only exists for T0 and T1.
+        u16 tnHold;
+        u16 sbus;
+    };
+    struct TimerTrapMask {
+        // When the upper part is 0xf, the int isn't masked
+        u8 overflowMask;
+        u8 compareMask;
+    };
+    enum GateMode {
+        ActivateGate,
+        ResetGateWhenHigh,
+        ResetGateWhenLow,
+        ResetGateWithDiffer,
+    };
+
     struct HwTimer {
         u32 clocks;
-        bool isEnabled;
-        u16 count;
-        bool gate;
+        // Count while gate not active
+        u32 count;
+        // All the EE timers could be deactivated by a locked gate
+        u16 gate;
+        GateMode gateMode;
+        bool withVbSync;
+        bool clearCountWithDiff;
 
+        operator bool() const {
+            return gate & 0xf || !isEnabled;
+        }
         vm::CallBackId callId;
         struct {
-            // Causes interruptions in the CPU when enabled
-            struct {
-                u16 comp;
-                // Only exists for T0 and T1.
-                u16 sbus;
-            } values;
-            struct {
-                bool overflow;
-                bool compare;
-            } trap;
-        } ctrl;
+            TimerInt values;
+            TimerTrapMask trap;
+            bool isEnabled;
+        };
     };
 
     class EeTimers {
     public:
         EeTimers(std::shared_ptr<vm::Scheduler>& solver, std::shared_ptr<console::IntCInfra>& inte);
         void resetTimers();
-
     private:
         std::shared_ptr<vm::Scheduler> scheduler;
         std::shared_ptr<console::IntCInfra> intc;
 
-        void timerReached(u8 raised, bool overflow);
+        void raiseClkTrigger(u8 raised, bool overflow);
+        void sysCtrlGate(bool hasVSync, bool high);
         vm::CallBackId raiseEvent{};
         std::array<HwTimer, 4> timers;
     };
