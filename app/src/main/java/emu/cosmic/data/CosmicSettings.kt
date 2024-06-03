773d7a6213dd86fd12147ae4679cbde55a5c45c7
Gabriel Correia <gabrielcorreia01012020@hotmail.com>
2024-06-03 15:50:35 -0300

    `Frontend`: We will use some settings from the frontend in the future

--- app/src/main/java/emu/cosmic/data/CosmicSettings.kt
+++ app/src/main/java/emu/cosmic/data/CosmicSettings.kt
@@ -4,37 +4,37 @@ import android.content.res.Resources.NotFoundException
 import emu.cosmic.CosmicApplication
 
 class CosmicSettings private constructor(context: Context) {
-    var appStorage by DelegateDataStore<String>(
-        SettingContainer(context, SettingsKeys.AppStorage))
-    var gpuTurboMode by DelegateDataStore<Boolean>(
-        SettingContainer(context, SettingsKeys.GpuTurboMode))
-    var customDriver by DelegateDataStore<String>(
-        SettingContainer(context, SettingsKeys.CustomDriver))
-    var eeMode by DelegateDataStore<Int>(
-        SettingContainer(context, SettingsKeys.EEMode))
-    var biosPath by DelegateDataStore<String>(
-        SettingContainer(context, SettingsKeys.BiosPath))
+    var appStorage by DelegateDataStore<String>(SettingContainer(context, SettingsKeys.AppStorage))
 
-    // Creating a static object to store all our configurations; this object will reside in the
-    // global heap memory (Accessible to JNI)
+    var gpuTurboMode by DelegateDataStore<Boolean>(SettingContainer(context, SettingsKeys.GpuTurboMode))
+
+    var customDriver by DelegateDataStore<String>(SettingContainer(context, SettingsKeys.CustomDriver))
+
+    var eeMode by DelegateDataStore<Int>(SettingContainer(context, SettingsKeys.EEMode))
+
+    var biosPath by DelegateDataStore<String>(SettingContainer(context, SettingsKeys.BiosPath))
+
+    // Creating a static object to store all our configurations
+    // This object will reside in the global heap memory (Accessible to JNI)
     companion object {
         val globalSettings by lazy { CosmicSettings(CosmicApplication.context) }
         var updateSettings: Boolean = false
-        @Suppress("unused") @JvmStatic
+
+        private val dsCachedSet = LinkedHashMap<String, Any>()
+        @JvmStatic
         fun getDataStoreValue(config: String) : Any {
-            return when (config) {
-                "dsdb_app_storage" -> globalSettings.appStorage
-                "dsdb_gpu_turbo_mode" -> globalSettings.gpuTurboMode
+            dsCachedSet.clear()
 
-                "dsdb_gpu_custom_driver" -> globalSettings.customDriver
-                "dsdb_ee_mode" -> globalSettings.eeMode
+            dsCachedSet["dsdb_app_storage"] = globalSettings.appStorage
+            dsCachedSet["dsdb_gpu_turbo_mode"] = globalSettings.gpuTurboMode
+            dsCachedSet["dsdb_gpu_custom_driver"] = globalSettings.customDriver
+            dsCachedSet["dsdb_ee_mode"] = globalSettings.eeMode
+            dsCachedSet["dsdb_bios_path"] = globalSettings.biosPath
 
-                "dsdb_bios_path" -> globalSettings.biosPath
-
-                else -> {
-                    throw NotFoundException(config)
-                }
+            if (!dsCachedSet.containsValue(config)) {
+                throw NotFoundException(config)
             }
+            return dsCachedSet.getValue(config)
         }
     }
 }
